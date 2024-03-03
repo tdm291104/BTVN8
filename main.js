77f8a7b684a034188e8b8d9e59c1b9de71789e8c
tdm <tu.manh291104@gmail.com>
2024-03-03 11:58:52 +0700

    update

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
 
+const userName = document.getElementById("name")
+const userEmail = document.getElementById("email")
+const userPws = document.getElementById("pws")
+
+function validateName(){
+    if(userName.value === ""){
+        userName.classList.add("loi")
+        return false
+    }else{
+        userName.classList.remove("loi")
+        return true
+    }
+}
+
+function validateEmail(){
+    if(userEmail.value === ""){
+        userEmail.classList.add("loi")
+        return false
+    }else{
+        userEmail.classList.remove("loi")
+        return true
+    }
+}
+
+function validatePws(){
+    if(userPws.value === ""){
+        userPws.classList.add("loi")
+        return false
+    }else{
+        userPws.classList.remove("loi")
+        return true
+    }
+}
+
+function validateForm(){
+    let check = true;
+
+    if (!validateName()) check = false
+    if (!validateEmail()) check = false
+    if (!validatePws()) check = false
+
+    return check
+}
+
 document.getElementById("submit").addEventListener("click", Submit)
 
 function Submit() {
-    let CheckSubmit = kiemtra();
+    let CheckSubmit = validateForm();
     if(CheckSubmit)
     {
     alert ("Đăng ký thành công")
     }
-}
-
-function kiemtra(){
-
-    let name = document.getElementById("name")
-    if(name.value == ""){
-        return false
-    }
-
-    let email = document.getElementById("email")
-    if(email.value == ""){
-        return false
-    }
-
-    let pws = document.getElementById("pws")
-    if(pws.value == ""){
-        return false
-    }
-
-    return true
 }
